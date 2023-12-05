11eca699691e77e406877684a00c579774759c93
Ben Schomp <ben@benschomp.com>
2023-12-04 19:06:51 -0500

    day 1, part 2 done

--- day-01.py
+++ day-01.py
@@ -1,24 +1,22 @@
-part_one = part_two = 0
 
 file = open('day-01.txt', 'r')
 for line in file:
-  sum = 0
+  part_one = part_two = 0
   line = line.strip()
   n = len(line)
 
   i = 0
   while i < n-1:
     if line[i] == line[i+1]:
-      sum += int(line[i])
-
+      part_one += int(line[i])
+    if line[i] == line[int((i+(n/2))%n)]:
+      part_two += int(line[i])
     i += 1
 
   if line[n-1] == line[0]:
-    sum += int(line[0])
-
-  print( line, sum )
+    part_one += int(line[0])
 
 file.close()
 
-print( "part_one:", sum )
+print( "part_one:", part_one )
 print( "part_two:", part_two )
