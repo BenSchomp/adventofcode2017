d3f25c8f2d7590c8c8e54e8dceb270a2578950a5
Ben Schomp <ben@benschomp.com>
2023-12-04 21:11:37 -0500

    day 4, part 2 done

--- day-04.py
+++ day-04.py
@@ -4,18 +4,35 @@ part_one = part_two = 0
 file = open('day-04.txt', 'r')
 for line in file:
   found_duplicate = False
+  found_anagram = False
   phrases = set()
+  anagrams = set()
+
   line = line.strip()
   words = line.split()
+
+  # --- part one ---
   for w in words:
     if w in phrases:
-      found_duplicate = True
+      found_duplicate = True 
       break
     else:
       phrases.add(w)
 
   if not found_duplicate:
     part_one += 1
+
+  # --- part two ---
+  for w in words:
+    w = ''.join(sorted(w))
+    if w in anagrams:
+      found_anagram = True
+      break
+    else:
+      anagrams.add(w)
+
+  if not found_anagram:
+    part_two += 1
 
 file.close()
 
