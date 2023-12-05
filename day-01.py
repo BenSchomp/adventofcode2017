b282ff2e9d1643c4ecb54f1902f9ddd8a1eaf593
Ben Schomp <ben@benschomp.com>
2023-12-04 19:08:48 -0500

    cleaned up loop, using % for part_one

--- day-01.py
+++ day-01.py
@@ -3,18 +3,15 @@ file = open('day-01.txt', 'r')
 for line in file:
   part_one = part_two = 0
   line = line.strip()
-  n = len(line)
 
   i = 0
-  while i < n-1:
-    if line[i] == line[i+1]:
+  n = len(line)
+  while i < n:
+    if line[i] == line[int(i+1)%n]:
       part_one += int(line[i])
     if line[i] == line[int((i+(n/2))%n)]:
       part_two += int(line[i])
     i += 1
-
-  if line[n-1] == line[0]:
-    part_one += int(line[0])
 
 file.close()
 
